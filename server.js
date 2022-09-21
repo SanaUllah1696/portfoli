3d6cd26c6dd1764db56368f4ca896020f342adc3
sana ullah <sanaullah1696@gmail.com>
2022-09-21 13:17:06 +0500

    script is added

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const bodyparser=require("body-parser");
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.use(express.static('public'))
 
 app.get("/",function(req,res){
@@ -7,9 +13,16 @@ app.get("/",function(req,res){
 
 });
 
+app.post("/",function(req,res){
+    const data=req.body;
+const v = confirm(data);
+}
+);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,function(){
     console.log("listen on >"+PORT+":");
+
 
 });
